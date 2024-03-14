ec0d217da26a24e579475b6ac367ec02c349aef8
Águeda Prieto Llamas <aguedapl55@educastur.es>
2024-03-14 20:48:46 +0100

    Creacion fragmentos principales Intento BottomNavigationView, maybe reemplazar con NavigationBarView??

--- app/src/main/java/fp/dam/proy/proy_dam/MainActivity.java
+++ app/src/main/java/fp/dam/proy/proy_dam/MainActivity.java
@@ -1,14 +1,50 @@
 package fp.dam.proy.proy_dam;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    CuentasFrag cuentasFrag = new CuentasFrag();
+    CategoriasFrag catFrag = new CategoriasFrag();
+    TransaccionesFrag transFrag = new TransaccionesFrag();
+    EstadisticasFrag estFrag = new EstadisticasFrag();
+    AjustesFrag ajFrag = new AjustesFrag();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView nav = findViewById(R.id.bottom_nav);
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.Cuentas:
+                        loadFragment(cuentasFrag);
+                        return true;
+
+                }
+                return false;
+            }
+        });
+
+        public void loadFragment(Fragment fragment) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame_container, fragment);
+            transaction.commit();
+
+        }
     }
+
+
 }
